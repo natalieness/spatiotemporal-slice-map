79e71bc40ea4198a59e354292c881e5dfcda2d96
Natalie Ness <68106552+natalieness@users.noreply.github.com>
2024-12-23 07:27:56 +0000

    Update convert_functions

--- python-scripts/convert_functions.py
+++ python-scripts/convert_functions.py
@@ -4,34 +4,42 @@ Helper functions to convert time formats, angle formats, etc.
 
 """
 
-import numpy as np
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.cluster import KMeans
+from scipy import signal
+from scipy.spatial.distance import cdist
+from sklearn.metrics import silhouette_samples, silhouette_score
+from matplotlib.mlab import detrend_linear
 
-def time_to_dec(times: list[float]) -> list[float]:
-    """
-    Converts all elements of a list from real time (HH.MM) to decimal hours.
-    
-    Parameters:
-    times (list of float): List of times in HH.MM format.
-    
-    Returns:
-    list of float: List of times in decimal hours.
-    """
-    times = np.array(times, dtype=float)
-    decimal_hours = np.floor(times) + (times % 1) / 0.6
-    return decimal_hours.tolist()
+from statsmodels.tsa.tsatools import detrend
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.ticker as ticker
+
+import os 
+import csv
 
 
-def dec_to_time(decimals: list[float]) -> list[float]:
-    """
-    Converts all elements of a list from decimal hours to real time (HH.MM).
-    
-    Parameters:
-    decimals (list of float): List of times in decimal hours.
-    
-    Returns:
-    list of float: List of times in HH.MM format.
-    """
-    decimals = np.array(decimals, dtype=float)
-    real_times = np.floor(decimals) + (decimals % 1) * 0.6
-    return real_times.tolist()
+def time_to_dec(tm):
+    '''
+    converts all elements of a list from real time to a decimal
+
+    '''
+    tm = [float(i) for i in tm] #make sure all elements are floats
+    dec = [(i-np.floor(i))/0.6+np.floor(i) for i in tm]
+    return dec 
+
+def dec_to_time(dec):
+    '''
+    converts all elements of a list from a decimal to real time
+
+    '''
+    dec = [float(i) for i in dec] #make sure all elements are floats
+    tm = [(i-np.floor(i))*0.6+np.floor(i) for i in dec]
+    return tm 
+
 
